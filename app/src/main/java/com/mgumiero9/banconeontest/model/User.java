40efc5e11fda45ac7f137b242224c2d31ba9dbe2
mgumiero9 <mgumiero9@gmail.com>
2017-03-24 20:00:15 -0300

    justCommitingCurrentWork

---
+++ app/src/main/java/com/mgumiero9/banconeontest/model/User.java
@@ -0,0 +1,54 @@
+package com.mgumiero9.banconeontest.model;
+
+/**
+ * Created by mgumiero9 on 24/03/17.
+ */
+
+public class User {
+
+    private String id;
+    private String nome;
+    private int telefone;
+    private String foto;
+    private String email;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
